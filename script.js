3608e3a04769c202f8f915a74909421ee8dbd736
Trifan <nicusormadalintrifan@yahoo.com>
2022-05-17 17:08:48 +0300

    Added script for right column

---
+++ script.js
@@ -0,0 +1,17 @@
+$('body')
+    .on('click', '.about-me-container, .education-container, .experience-container', function() {
+        $('body').css('overflow"', "yellow");
+        jQuery('<div>', {
+            class: 'highlighted-container',
+        }).appendTo('body');
+        let $clone = $(this).clone();
+        $clone.addClass('highlighted-hover');
+        let $container = $('.highlighted-container');
+        $container.html($clone);
+        $container.show(300);
+        $("html, body").animate({ scrollTop: 0 }, 10);
+    })
+    .on('click', '.highlighted-hover', function() {
+        $('.highlighted-container').remove();
+        $('body').css('overflow', 'auto');
+    });
